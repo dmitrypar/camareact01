b062863084fdaff0d59e824ca62532eec3c8c7d4
dmitriyPC <dmitrypn81@gmail.com>
2019-12-24 15:28:55 +0300

    finish part on JS+redux

---
+++ src/index.js
@@ -0,0 +1,77 @@
+import {createStore, bindActionCreators} from 'redux';
+import * as actions from './actions'
+import reducer from './reducer'
+
+const store = createStore(reducer);
+const {dispatch} = store;
+
+/*const inc = () => {
+	return {type: 'INC'}
+};*/
+
+/*const incDispatch = () => dispatch(inc());
+const decDispatch = () => dispatch(dec());
+const rndDispatch = (payload) => dispatch(rnd(payload));*/
+
+/*const bindActionCreator = (creator, dispatch) => (...args) => {
+    dispatch(creator(...args))
+};
+// так работает функция bindActionCreators from redux
+
+
+const incDispatch = bindActionCreator(inc, dispatch);
+const decDispatch = bindActionCreator(dec, dispatch);
+const rndDispatch =  bindActionCreator(rnd, dispatch);*/
+
+/*const incDispatch = bindActionCreators(inc, dispatch);
+const decDispatch = bindActionCreators(dec, dispatch);
+const rndDispatch =  bindActionCreators(rnd, dispatch);*/
+
+const {inc,dec, rnd } = bindActionCreators (actions, dispatch);
+
+
+document
+.getElementById('inc')
+.addEventListener('click',inc);
+
+
+document
+.getElementById('dec')
+.addEventListener('click', dec);
+
+document
+    .getElementById('rnd')
+    .addEventListener('click', ()=>{
+    	const payload = Math.floor(Math.random()*10);
+        rnd(payload);
+    });
+
+const update = ()  => {
+document
+.getElementById('counter')
+.innerHTML= store.getState()
+};
+
+store.subscribe(update);
+
+
+
+/*let state = reducer(undefined, {})
+
+state = reducer(state, {type:'INC'});
+console.log(state);
+
+state = reducer(state, {type:'INC'});
+
+console.log(state);*/
+
+
+
+/*store.subscribe(()=>{
+	console.log(store.getState());
+});
+
+console.log(store.getState());
+
+store.dispatch({type: 'INC'});
+store.dispatch({type: 'INC'});*/
